dd94afc095d892168247294c4fd3c8ff3e9a070e
Samie-ub <wuzsamie@gmail.com>
2025-12-02 01:11:39 +0500

    public-file-update

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -10,7 +10,7 @@ const  HomeHeroText = () => {
             <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
                 qui
                 <div className='sm:h-[7vw] h-12 sm:w-[16vw] w-22 sm:mb-0 mb-2 rounded-full  overflow-hidden '>
-                    <Video src={"/public/home1.mp4"} />
+                    <Video src={"/home1.mp4"} />
                 </div>
                 génère
             </div>
